ae39ad64c390c54668c184152fc581869c38e4d0
bhendi-boi <bhendiisgreen@gmail.com>
2024-10-28 18:48:32 +0530

    added waveform dump

--- testbench.sv
+++ testbench.sv
@@ -9,30 +9,35 @@ import uvm_pkg::*;
 `include "random_test.sv"
 `include "interface.sv"
 
-module top();
+module top ();
 
-    logic clk;
-    initial begin
-        clk = 0;
-    end
-    always #10 clk = ~clk;
+  logic clk;
+  initial begin
+    clk = 0;
+  end
+  always #10 clk = ~clk;
 
-    divider_intf intf(.clk(clk));
+  divider_intf intf (.clk(clk));
 
-    divider_dshift dut(
-        .i_clk(intf.clk),
-        .i_rst(intf.rst),
-        .i_dividend(intf.dividend),
-        .i_divisor(intf.divisor),
-        .i_start(intf.start),
-        .o_ready(intf.ready),
-        .o_quotient(intf.quotient),
-        .o_remainder(intf.remainder)
-    );
+  divider_dshift dut (
+      .i_clk(intf.clk),
+      .i_rst(intf.rst),
+      .i_dividend(intf.dividend),
+      .i_divisor(intf.divisor),
+      .i_start(intf.start),
+      .o_ready(intf.ready),
+      .o_quotient(intf.quotient),
+      .o_remainder(intf.remainder)
+  );
 
-    initial begin
-        uvm_config_db#(virtual divider_intf)::set(null,"*","vif",intf);
-        run_test("random_test");
-    end
+  initial begin
+    uvm_config_db#(virtual divider_intf)::set(null, "*", "vif", intf);
+    run_test("random_test");
+  end
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars();
+  end
 
 endmodule
