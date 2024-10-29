658c99f7a6d776c6c78a2265c1002fcb6fa4e0ab
bhendi-boi <bhendiisgreen@gmail.com>
2024-10-29 21:23:27 +0530

    updated environment which includes coverage

--- environment.sv
+++ environment.sv
@@ -3,6 +3,7 @@ class environment extends uvm_env;
 
   agent agnt;
   scoreboard scb;
+  coverage cov;
 
   function new(string name = "environment", uvm_component parent);
     super.new(name, parent);
@@ -12,11 +13,13 @@ class environment extends uvm_env;
     super.build_phase(phase);
     agnt = agent::type_id::create("agnt", this);
     scb  = scoreboard::type_id::create("scb", this);
+    cov  = coverage::type_id::create("cov", this);
   endfunction
 
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
     agnt.mon.monitor_port.connect(scb.scoreboard_port);
+    agnt.mon.monitor_port.connect(cov.coverage_port);
   endfunction
 
 endclass
