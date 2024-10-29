5ad22f736465b65f472bf5cb1d948a96c9050410
bhendi-boi <bhendiisgreen@gmail.com>
2024-10-29 21:21:36 +0530

    added setup for coverage with imc

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 rm -rf *log
 rm -rf xcelium.d
 rm -rf xrun.*
-
+rm -rf cov_work
+rm -rf coverage_random.rpt
 echo "Cleaned"
---
+++ imc.tcl
@@ -0,0 +1,3 @@
+load -run test_random
+report_metrics -detail -out coverage_random.rpt -metrics all -source on
+# quit
